d74fe09d95adc63276e8aa005f34db7f4c48b393
程子丘 <jxlpzqc@live.com>
2024-11-24 18:41:01 +0800

    feat: use individual text logo

--- client/renderer/src/components/FailPage.tsx
+++ client/renderer/src/components/FailPage.tsx
@@ -7,7 +7,7 @@ type Props = {
 
 export default function ({ reason, buttonTitle, onButtonClick, noPage }: Props) {
   return <div className={`flex flex-col w-full ${!noPage ? "h-screen" : ""} gap-8 items-center justify-center`}>
-    <img src='/images/lol_icon.png' className='w-40 h-40' />
+    <img src='/images/lop-logo-text.png' className='h-40' />
     <div className='flex items-center justify-center'>
       <div className='text-white p-4 rounded-lg'>
         {reason}
