9339cbd3577647714bf9332462791d0206d54640
程子丘 <jxlpzqc@live.com>
2024-12-13 17:22:59 +0800

    chore: add sql log

--- server/src/prisma.service.ts
+++ server/src/prisma.service.ts
@@ -3,6 +3,11 @@ import { PrismaClient } from '@prisma/client'
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit {
+  constructor() {
+    super({
+      log: ['query']
+    })
+  }
   async onModuleInit() {
     // Note: this is optional
     await this.$connect()
