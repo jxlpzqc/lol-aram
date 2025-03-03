64340998b66916803375681daa70bc6f4121bf7c
程子丘 <jxlpzqc@live.com>
2025-03-03 21:37:08 +0800

    fix: loading page scroll bar

--- client/renderer/src/components/LoadingPage.tsx
+++ client/renderer/src/components/LoadingPage.tsx
@@ -7,7 +7,7 @@ type Props = {
 export default function (props: Props) {
   return <div className={`flex flex-col w-full ${!props.noPage ? "h-screen" : ""} gap-8 items-center justify-center`}>
     <img src='/images/lop-logo-text.png' className='h-40' />
-    <div className='flex items-center justify-center'>
+    <div className='flex items-center justify-center mb-8'>
       <img src='/images/spinner.png' className='w-16 h-16 animate-spin' />
       <div className='text-white p-4 rounded-lg'>
         {props.message || "连接中..."}
